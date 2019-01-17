80665972a6c3bb89f3fd4584cfc348b6e5d6c5c8
Claudio Junior <cso.junior1996@gmail.com>
2019-01-17 12:47:58 -0200

    Aula: Estilizando abas

--- src/pages/organizations/index.js
+++ src/pages/organizations/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Header from '~/components/Header';
 
 // import styles from './styles';
 
 const Organizations = () => (
   <View>
-    <Text>Organizations</Text>
+    <Header title="Organizações" />
   </View>
 );
+
+const TabIcon = ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />;
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Organizations.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Organizations;
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,30 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import Welcome from '~/pages/welcome';
 import Repositories from '~/pages/repositories';
+import Organizations from '~/pages/organizations';
+
+import { colors } from '~/styles';
 
 const Routes = (userLogged = false) => createAppContainer(
   createSwitchNavigator({
     Welcome,
-    Repositories,
+    User: createBottomTabNavigator({
+      Repositories,
+      Organizations,
+    }, {
+      tabBarOptions: {
+        showIcon: true,
+        showLabel: false,
+        activeTintColor: colors.white,
+        inactiveTintColor: colors.whiteTransparent,
+        style: {
+          backgroundColor: colors.secundary,
+        },
+      },
+    }),
   }, {
-    initialRouteName: userLogged ? 'Repositories' : 'Welcome',
+    initialRouteName: userLogged ? 'User' : 'Welcome',
   }),
 );
 
