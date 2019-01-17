f600e47032ba11a1fc41937693ce7205cff5e2fc
Claudio Junior <cso.junior1996@gmail.com>
2019-01-17 03:04:00 -0200

    Aula: Configurando Header

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import Header from '~/components/Header';
 
 // import styles from './styles';
 
 const Repositories = () => (
   <View>
-    <Text>Repositories</Text>
+    <Header title="Repositórios" />
   </View>
 );
 
