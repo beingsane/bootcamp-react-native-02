075bca0857b95a44f8bbf424fc200a2079057ac3
Claudio Junior <cso.junior1996@gmail.com>
2019-01-17 02:28:11 -0200

    Aula: Estilizando Welcome

--- src/styles/metrics.js
+++ src/styles/metrics.js
@@ -3,8 +3,8 @@ import { Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 export default {
-  basePadding: 10,
-  baseMargin: 20,
+  basePadding: 20,
+  baseMargin: 10,
   baseRadius: 5,
   screenWidth: width < height ? width : height,
   screenHeight: width < height ? height : width,
