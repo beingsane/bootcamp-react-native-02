3b1d4c7d69e4074e84ec3f856a4b165a0d241e31
Claudio Junior <cso.junior1996@gmail.com>
2019-01-17 02:52:46 -0200

    Aula: Definindo rota inicial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '~/config/ReactotronConfig';
 import '~/config/DevToolsConfig';
 
-import Routes from './routes';
+import { AsyncStorage } from 'react-native';
 
-const App = () => <Routes />;
+import createNavigator from './routes';
 
-export default App;
+export default class App extends Component {
+  state = {
+    userChecked: false,
+    userLogged: false,
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+
+    this.setState({ userChecked: true, userLogged: !!username });
+  }
+
+  render() {
+    const { userChecked, userLogged } = this.state;
+
+    if (!userChecked) return null;
+
+    const Routes = createNavigator(userLogged);
+
+    return <Routes />;
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import Welcome from '~/pages/welcome';
 import Repositories from '~/pages/repositories';
 
-const Routes = createAppContainer(
+const Routes = (userLogged = false) => createAppContainer(
   createSwitchNavigator({
     Welcome,
     Repositories,
+  }, {
+    initialRouteName: userLogged ? 'Repositories' : 'Welcome',
   }),
 );
 
